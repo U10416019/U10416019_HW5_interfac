980e83c35e8cc7ddc2950c7a8bc7f981f5afebd2
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-19 10:38:18 +0800

    Model is the exterior of the bean game
    This file is just has the exterior lines of the bean game, and the lines are calculate the coordinate on the pane.  After create all lines in the bean game, add the lines in the pane.

--- BeanGame.java
+++ BeanGame.java
@@ -23,7 +23,45 @@ public class BeanGame extends Application{
 		pane.getChildren().add(bleft);
 		pane.getChildren().add(bright);
 		
+		
+		
+		//create seven button lines
+		Line b1 = new Line(85, 380, 85, 315);
+		Line b2 = new Line(120, 380, 120, 315);
+		Line b3 = new Line(155, 380, 155, 315);
+		Line b4 = new Line(190, 380, 190, 315);
+		Line b5 = new Line(225, 380, 225, 315);
+		Line b6 = new Line(260, 380, 260, 315);
+		Line b7 = new Line(295, 380, 295, 315);
+		//add lines in pane
+		pane.getChildren().add(b1);
+		pane.getChildren().add(b2);
+		pane.getChildren().add(b3);
+		pane.getChildren().add(b4);
+		pane.getChildren().add(b5);
+		pane.getChildren().add(b6);
+		pane.getChildren().add(b7);
+		
+		
+		
+		//create top left and top right line
+		Line topLeft = new Line(172.5, 15, 172.5, 55);
+		Line topRight = new Line(207.5, 15, 207.5, 55);
+		//add lines in pane
+		pane.getChildren().add(topLeft);
+		pane.getChildren().add(topRight);
+		
+		
+		
+		//create right and left slash lines
+		Line left = new Line(50, 315, 172.5, 55);
+		Line right = new Line(330, 315, 207.5, 55);
+		//add lines in pane
+		pane.getChildren().add(left);
+		pane.getChildren().add(right);
+		
 
+		
 		//Create a scene and place it in the stage
 		Scene scene = new Scene(pane, 380, 410);
 		primaryStage.setTitle("U10416019");
