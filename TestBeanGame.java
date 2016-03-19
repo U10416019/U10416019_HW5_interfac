5ed93e559ef4249b2c4cb35f9e3fc4487f70248b
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-19 10:56:38 +0800

    Main class of the bean game
    This file is the main class of the bean game, and this file just import the application app of java, then call the bean game class.

---
+++ TestBeanGame.java
@@ -0,0 +1,10 @@
+//U10416019鄭文琪
+
+import javafx.application.Application;
+
+public class TestBeanGame{
+	public static void main(String[] args){
+		//call the BeanGame class
+		Application.launch(BeanGame.class, args);
+	}
+}
